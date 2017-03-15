5ea97518bccbb09987d6b7c7e5529a691c22bdf4
hanax <alfalfa.xue@gmail.com>
2017-03-14 23:30:23 -0400

    implement code

--- lifting/lifting.ml
+++ lifting/lifting.ml
@@ -47,14 +47,71 @@ let rec is_target_prog (e : exp) : bool =
    the functions converted *so far*. You will want to add new bindings to s
    and return the expanded mapping along with the converted expression. *)
 let convert (e : exp) (s : state) : exp * state =
-  failwith "Implement me!"
-  (* Remember: the `Letrec` case is a karma problem (worth zero points). If
-     you are not attempting that part, you can just use "failwith" to bail out
-     if you encounter a `Letrec` in the AST. *)
+  let fr = Fresh.make (allv e)
+  in let rec helper (ee : exp) (ss : state) : exp * state =
+    match ee with
+    | Var _ -> (ee, ss)
+    | Fun (x, ee) -> let (eee, sss) = helper ee ss
+      in let fv_l = HashSet.values (fv (Fun (x, eee))) in let f = Fresh.next fr
+      in let rec app_l ff f_l = match f_l with 
+        | [] -> ff
+        | h :: l -> app_l (App (ff, Var h)) l
+      in (app_l (Var f) fv_l, update sss f (Closure (Fun (List.append fv_l x, eee), make ())))
+    | Let ([i], ee1, ee2) -> helper (App (Fun ([i], ee2), ee1)) ss
+    | Let (h :: l, ee1, ee2) -> helper (Let ([h], Fun (l, ee1), ee2)) ss 
+    | Let _ -> failwith "wrong format" 
+    | Letrec _ -> failwith "letrec not implemented" 
+    | App (e1, e2) -> let (ee1, ss1) = helper e1 ss 
+      in let (ee2, ss2) = helper e2 ss1 
+      in (App (ee1, ee2), ss2)
+    | Cond (b, e1, e2) -> let (bb, ss1) = helper b ss 
+      in let (ee1, ss2) = helper e1 ss1 
+      in let (ee2, ss3) = helper e2 ss2 
+      in (Cond (bb, ee1, ee2), ss3)
+    | Num _ -> (ee, ss)
+    | Plus (n1, n2) -> let (nn1, ss1) = helper n1 ss 
+      in let (nn2, ss2) = helper n2 ss1 
+      in (Plus (nn1, nn2), ss2)
+    | Minus (n1, n2) -> let (nn1, ss1) = helper n1 ss 
+      in let (nn2, ss2) = helper n2 ss1 
+      in (Minus (nn1, nn2), ss2)
+    | Times (n1, n2) -> let (nn1, ss1) = helper n1 ss 
+      in let (nn2, ss2) = helper n2 ss1 
+      in (Times (nn1, nn2), ss2)
+    | Div (n1, n2) -> let (nn1, ss1) = helper n1 ss 
+      in let (nn2, ss2) = helper n2 ss1 
+      in (Div (nn1, nn2), ss2)
+    | Mod (n1, n2) -> let (nn1, ss1) = helper n1 ss 
+      in let (nn2, ss2) = helper n2 ss1 
+      in (Mod (nn1, nn2), ss2)
+    | And (b1, b2) -> let (bb1, ss1) = helper b1 ss 
+      in let (bb2, ss2) = helper b2 ss1 
+      in (And (bb1, bb2), ss2)
+    | Or (b1, b2) -> let (bb1, ss1) = helper b1 ss 
+      in let (bb2, ss2) = helper b2 ss1 
+      in (Or (bb1, bb2), ss2)
+    | Not b -> let (bb, ss1) = helper b ss 
+      in (Not bb, ss1)
+    | Eq (b1, b2) -> let (bb1, ss1) = helper b1 ss 
+      in let (bb2, ss2) = helper b2 ss1 
+      in (Eq (bb1, bb2), ss2)
+    | Leq (b1, b2) -> let (bb1, ss1) = helper b1 ss 
+      in let (bb2, ss2) = helper b2 ss1 
+      in (Leq (bb1, bb2), ss2)
+    | Lt (b1, b2) -> let (bb1, ss1) = helper b1 ss 
+      in let (bb2, ss2) = helper b2 ss1 
+      in (Lt (bb1, bb2), ss2)
+    | Bool _ -> (ee, ss)
+  in helper e s
 
 (* Convert a source FL expression into a complete, self-contained expression
    in the target version of FL. This function should call `convert` to get the
    new expression and the resulting function bindings. It should then turn all
    the bindings into `let` expressions to compose the complete program. *)
 let lift (e : exp) : exp =
-  failwith "Implement me!"
+  let rec helper (ee : exp) (s : (id * value) list) : exp =
+    match s with
+    | (i, Closure (eee, _)) :: l -> helper (Let ([i], eee, ee)) l
+    | [] -> ee
+    | _ -> failwith "wrong format"
+  in let (ee, s) = convert e (make ()) in helper ee (bindings s)
